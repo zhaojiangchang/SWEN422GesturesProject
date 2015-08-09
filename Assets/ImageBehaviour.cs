73801f4f47d036039b4b57e39a4603f1caaf7026
supremekai <beauchjord@gmail.com>
2015-08-10 01:40:45 +1200

    ZOoming

--- Assets/ImageBehaviour.cs
+++ Assets/ImageBehaviour.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using Leap;
+using System.Diagnostics;
+using System.Linq;
 
 public class ImageBehaviour : MonoBehaviour {
 
 	Controller controller;
 	private static List<Rigidbody2D> images = new List<Rigidbody2D>();
 	Rigidbody2D image;
+	float panelWidth = 780;
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +21,34 @@ public class ImageBehaviour : MonoBehaviour {
 		controller.EnableGesture (Gesture.GestureType.TYPEKEYTAP);
 		
 		// Adjust screen tap sensitivity etc.
-		controller.Config.SetFloat ("Gesture.KeyTap.MinDownVelocity", 10.0f);
+		controller.Config.SetFloat ("Gesture.KeyTap.MinDownVelocity", 20.0f);
 		controller.Config.SetFloat ("Gesture.KeyTap.HistorySeconds", 0.2f);
-		controller.Config.SetFloat ("Gesture.KeyTap.MinDistance", 1.0f);
+		controller.Config.SetFloat ("Gesture.KeyTap.MinDistance", 4.0f);
 		controller.Config.Save();
+	}
+
+	float enterZ;
+	Stopwatch timer;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Frame frame = controller.Frame ();
+		Hand hand = frame.Hands[0];
+		enterZ = hand.Fingers [0].TipPosition.z;
+		timer = new Stopwatch ();
+		timer.Start ();
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		timer.Stop ();
 	}
 
 	void OnTriggerStay2D(Collider2D other) 
 	{
 		Frame frame = controller.Frame ();
-		if (frame.Hands [0].PinchStrength > 0.6f) 
+		Hand hand = frame.Hands[0];
+
+		if (hand.PinchStrength > 0.6f) 
 		{
 			print ("Selected: " + this.gameObject.name);
 			if(!images.Contains(image))
@@ -35,6 +56,19 @@ public class ImageBehaviour : MonoBehaviour {
 				print ("Added: " + this.gameObject.name);
 				images.Add(image);
 			}
+		}
+
+		if (frame.Gestures () [0].Type == Gesture.GestureType.TYPEKEYTAP) 
+		{
+			print ("Screen tap");
+			timer.Reset();
+		}
+
+		// If nothing is selected and curosr is hovering over an image.
+		if(images.Count == 0 && timer.ElapsedMilliseconds > 1500) 
+		{
+			float z = hand.Fingers[0].TipPosition.z > enterZ  ? 0.5f : -0.5f;
+			transform.localScale = new Vector2(transform.localScale.x - z, transform.localScale.y - z);
 		}
 	}
 
@@ -48,6 +82,7 @@ public class ImageBehaviour : MonoBehaviour {
 			print ("Screen tap");
 			images.Clear ();
 		}
+
 		if(images.Contains(image))
 			trackLeap (frame);
 	}
@@ -55,7 +90,8 @@ public class ImageBehaviour : MonoBehaviour {
 	void trackLeap(Frame frame)
 	{	
 		int sep = images.IndexOf (image);
-		int offset = 50;
+		float xOffset = transform.localScale.x * 1.4f;
+		float yOffset = transform.localScale.y + 4.0f;
 		// Cursor follow LeapMotion hand position.
 		Hand hand = frame.Hands [0];
 		Vector3 v = hand.StabilizedPalmPosition.ToUnity();
@@ -74,11 +110,11 @@ public class ImageBehaviour : MonoBehaviour {
 		v.y = ((v.y - 100) / 150) * UnityEngine.Screen.height;
 		
 		// Limit cursor draw range i.e. Keep cursor inside window.
-		v.x = Mathf.Clamp (v.x, offset, UnityEngine.Screen.width - offset);
-		v.y = Mathf.Clamp (v.y, offset, UnityEngine.Screen.height - offset);
+		v.x = Mathf.Clamp (v.x, xOffset, panelWidth - xOffset);
+		v.y = Mathf.Clamp (v.y + 4.0f, yOffset, UnityEngine.Screen.height - yOffset);
 		
 		Vector3 z = Camera.main.ScreenToWorldPoint (v);
 		
-		image.position = new Vector2 (z.x, (z.y + (sep * 500)));
+		image.position = new Vector2 (z.x, (z.y + (sep * yOffset * 10f)));
 	}
 }
